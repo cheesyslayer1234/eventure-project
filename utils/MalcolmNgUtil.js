7ec2103b2997e2f40049430081ed31b2fd68f05a
cheesyslayer1234 <2401779J@student.tp.edu.sg>
2025-11-21 15:26:50 +0800

    Implemented the backend functionality for add-event
    I implemented the backend functionality for add-event so that the create event functionality can work, posting the event details to the JSON database.

---
+++ utils/MalcolmNgUtil.js
@@ -0,0 +1,44 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { Event } = require('../models/Event');
+
+const RESOURCES_FILE = path.join(__dirname, '../utils/event.json');
+const TEMPLATE_FILE = path.join(__dirname, '../utils/event.template.json');
+
+async function addEventlisting(req, res) {
+    try {
+        const { name, description, date, time, location, image } = req.body;
+
+        if (!name || !description || !date || !time || !location || !image) {
+            return res.status(400).json({ message: "All fields are required." });
+        }
+
+        const newEvent = new Event(name, description, date, time, location, image);
+
+        let resources = [];
+
+        try {
+            const data = await fs.readFile(RESOURCES_FILE, 'utf8');
+            resources = JSON.parse(data);
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                const templateData = await fs.readFile(TEMPLATE_FILE, 'utf8');
+                resources = JSON.parse(templateData);
+                await fs.writeFile(RESOURCES_FILE, JSON.stringify(resources, null, 2));
+            } else {
+                throw err;
+            }
+        }
+
+        resources.push(newEvent);
+        await fs.writeFile(RESOURCES_FILE, JSON.stringify(resources, null, 2));
+
+        return res.status(201).json(newEvent);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = { addEventlisting };
